a2ca6f35371f9573d626b8cf468ff726a5d31771
Claudio Bisegni <claudio.bisegni@lnf.infn.it>
2021-10-16 12:45:15 +0200

    fixes

--- docker-compose/init-db/10-create-table.sql
+++
@@ -1,6 +0,0 @@
--- Creation of test table
-CREATE TABLE IF NOT EXISTS test_data (
-  id INT NOT NULL,
-  name varchar(250) NOT NULL,
-  PRIMARY KEY (id)
-);
--- docker-compose/init-db/20-fill-table.sql
+++
@@ -1,7 +0,0 @@
--- Set params
-set session my.number_of_test_data = '300';
-
--- Filling of test_data
-INSERT INTO test_data
-select id, concat('data ', id) 
-FROM GENERATE_SERIES(1, 300) as id;
